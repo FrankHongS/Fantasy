7bc7a34a193c64a4f0718448c1b8a8e504cca1c5
frankhon <frank_hon@foxmail.com>
2020-04-17 00:29:37 +0800

    new Fantasy

--- app/src/main/java/com/frankhon/fantasymusic/MainActivity.kt
+++ app/src/main/java/com/frankhon/fantasymusic/MainActivity.kt
@@ -2,11 +2,24 @@ package com.frankhon.fantasymusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_haha.setOnClickListener(this)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_container, FragmentA())
+            .commit()
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            tv_haha -> Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show()
+        }
     }
 }
